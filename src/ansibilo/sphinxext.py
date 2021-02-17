66278ae83e9cdf17554caebb9086e4b77472e5b8
Joffrey MANDER <joffrey.mander@polyconseil.fr>
2021-02-17 15:49:01 +0100

    sphinxext: add new parameter 'filename' from sphinx (version 3.5.1)

--- src/ansibilo/sphinxext.py
+++ src/ansibilo/sphinxext.py
@@ -17,8 +17,14 @@ class AnsibleInventoryGraphDirective(sphinx_graphviz.Graphviz):
 
 def render_dot_decorator(fct):
     @functools.wraps(fct)
-    def inner(self, code, options, output_format, prefix):
-        rel_filename, out_filename = fct(self, code, options, output_format, prefix)
+    def inner(self, code, options, output_format, prefix, filename=None):
+        # sphinx version 3.5.1 added a new parameter filename
+        # this condition try to make this patched function retrocompatible
+        if filename is not None:
+            rel_filename, out_filename = fct(self, code, options, output_format, prefix, filename)
+        else:
+            rel_filename, out_filename = fct(self, code, options, output_format, prefix)
+
         if output_format == 'svg' and exporters.ANSIBLE_INVENTORY_TAG in code:
             # Drop svg width and height so the svg will fit in the content block
             tree = dom.parse(out_filename)
